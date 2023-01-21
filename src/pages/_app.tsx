f6610b571b801a2beef2628ad1e935adc49a6a0f
Hitesh Chhatvani <hiteshchhatvani@gmail.com>
2023-01-21 14:50:27 +0530

    feat: init apollo client in frontend

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,20 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { SessionProvider } from "next-auth/react";
 import { theme } from "../chakra/theme";
 import type { AppProps } from "next/app";
+import { client } from "../graphql/apollo-client";
+import { ApolloProvider } from "@apollo/client";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </SessionProvider>
+    <ApolloProvider client={client}>
+      <SessionProvider session={session}>
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </SessionProvider>
+    </ApolloProvider>
   );
 }
