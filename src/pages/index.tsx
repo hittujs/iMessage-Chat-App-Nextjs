22427ae5ffb022a946dcc9be0573097c3aa7b64c
Hitesh Chhatvani <hiteshchhatvani@gmail.com>
2023-01-21 12:36:52 +0530

    chore: add user to database

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
+import { signIn, useSession } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div>Hello from home page</div>;
+  const { data, status } = useSession();
+
+  return (
+    <div>
+      <button onClick={() => signIn("google")}>Sign In User</button>
+    </div>
+  );
 }
